ce1de7ecadfdd6d7c0f69bc143a3ef1de7a1c656
Nandini <nandini.kaviskar@gmail.com>
2017-12-14 00:17:53 -0800

    this is my first commit

---
+++ ANTProject/src/sample.java
@@ -0,0 +1,12 @@
+
+public class sample {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println( "Hello world ..... !");
+		Thread.sleep(5000);
+		System.out.println("Exceuted Ant project successfully ");
+		Thread.sleep(5000);
+	}
+
+}
